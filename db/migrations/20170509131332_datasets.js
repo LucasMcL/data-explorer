07e03a79ea59f935c6a121000f77773391f1cf39
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-09 15:09:19 -0500

    migration and seed files

---
+++ db/migrations/20170509131332_datasets.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('datasets', table => {
+		table.increments()
+		table.text('data', 'longtext').notNullable()
+		table.string('name')
+		table.string('file_type')
+		table.string('size')
+
+		table.integer('user_id').unsigned().references('users.id')
+	})
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('datasets')
+};
