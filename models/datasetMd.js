47e2a12d94fcf8f30593ba57e9a1a3c8191841c3
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-10 11:50:01 -0500

    add GET and POST methods on dataset model

--- models/datasetMd.js
+++ models/datasetMd.js
@@ -8,6 +8,15 @@ const Dataset = bookshelf.Model.extend({
 	graphs: function() { return this.hasMany('Graph') }
 }, {
 	dependents: ['graphs'],
+	getAll: function() {
+		return this.forge().fetchAll()
+	},
+	getAllForUser: function(user_id) {
+		return this.forge().where({user_id}).fetchAll()
+	},
+	getOneById: function(datasetId) {
+		return this.forge().where({id: datasetId}).fetch()
+	}
 })
 
 module.exports = bookshelf.model('Dataset', Dataset)
