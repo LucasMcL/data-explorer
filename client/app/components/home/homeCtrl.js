3f6ed42bd74fb4a0d7b1ed79965f09e3a9c19bd7
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-10 16:56:27 -0500

    ui grid wip

--- client/app/components/home/homeCtrl.js
+++ client/app/components/home/homeCtrl.js
@@ -1,3 +1,24 @@
 app.controller('HomeCtrl', function($scope) {
 	console.log('Home control instantiated')
+
+	$scope.myData = [
+    {
+        "firstName": "Cox",
+        "lastName": "Carney",
+        "company": "Enormo",
+        "employed": true
+    },
+    {
+        "firstName": "Lorraine",
+        "lastName": "Wise",
+        "company": "Comveyer",
+        "employed": false
+    },
+    {
+        "firstName": "Nancy",
+        "lastName": "Waters",
+        "company": "Fuelton",
+        "employed": false
+    }
+	]
 })
