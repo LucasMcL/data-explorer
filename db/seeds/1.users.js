aad299f0e0373aa63d01951e830eb8639be2c816
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-10 11:49:02 -0500

    added second user for testing purposes

--- db/seeds/1.users.js
+++ db/seeds/1.users.js
@@ -5,6 +5,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users').insert([
         {email: 'testuser@example.com', hash: 'abc123'},
+        {email: 'testuser2@example.com', hash: 'def456'}
       ]);
     });
 };
