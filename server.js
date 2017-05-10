3a5a832236eb7dd84b4117e0c1ef2609050610e0
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-10 11:50:23 -0500

    mount body parser and change port # when in test mode

--- server.js
+++ server.js
@@ -3,15 +3,45 @@
 // Require packages
 const express = require('express')
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 
-// Config
-dotenv.config()
-const PORT = process.env.PORT || 8000
+// Require routes, gathered in index.js file
+const routes = require('./routes/')
 
 // Init app
 const app = express()
 
+// Config
+dotenv.config()
+let PORT = process.env.PORT || 8000
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+// API routes
+app.use('/api/', routes)
+
+app.use((err,req,res,next) => {
+  console.log("error", err)
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: {}
+  })
+})
+
+if(process.env.NODE_ENV === 'test') PORT = 9001
 app.listen(PORT, function() {
 	console.log(`back end application listening on port ${PORT}`)
 })
+
+module.exports = app
+
+
+
+
+
+
+
+
 
