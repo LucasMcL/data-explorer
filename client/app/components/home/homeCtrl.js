4c315215a2359162ac6a7854ffb050e156332ee9
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-11 12:10:47 -0500

    user can load in csv file and see data displayed in grid.  Need to deal with bug relating to dot notation in column names

--- client/app/components/home/homeCtrl.js
+++ client/app/components/home/homeCtrl.js
@@ -1,24 +1,97 @@
-app.controller('HomeCtrl', function($scope) {
-	console.log('Home control instantiated')
+app.controller('HomeCtrl', function($scope, uiGridConstants) {
+  console.log('Home control instantiated')
 
-	$scope.myData = [
-    {
-        "firstName": "Cox",
-        "lastName": "Carney",
-        "company": "Enormo",
-        "employed": true
-    },
-    {
-        "firstName": "Lorraine",
-        "lastName": "Wise",
-        "company": "Comveyer",
-        "employed": false
-    },
-    {
-        "firstName": "Nancy",
-        "lastName": "Waters",
-        "company": "Fuelton",
-        "employed": false
+  // Read file on file input event
+  $('#file-input').change(upload)
+
+  $scope.grid = {
+    columnDefs: [],
+    data: [],
+    onRegisterApi: function(gridApi) {
+      $scope.gridApi = gridApi
     }
-	]
+  }
+
+  // Read CSV file, convert to array of objects and attach to scope
+  // If first var is blank, replace with var name "row"
+  function upload(evt) {
+    console.log('upload')
+    let file = evt.target.files[0]
+    let reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = function(event) {
+      let csvData = event.target.result
+      if(csvData.slice(0, 3) === `"",`) {
+        csvData = `"row",` + csvData.slice(3)
+      }
+      const parsedData = $.csv.toObjects(csvData)
+      $scope.grid.data = parsedData
+      const varNames = Object.keys(parsedData[0])
+      $scope.grid.columnDefs = []
+      varNames.forEach(name => $scope.grid.columnDefs.push({field: name}))
+      $scope.$apply()
+      $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.ALL )
+    }
+    reader.onerror = function() {
+      console.log('error reading file')
+    }
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const stringData = JSON.stringify(dataAsObjects)
+  // $.ajax({
+  //   type: "POST",
+  //   url: 'http://localhost:5555/files',
+  //   data: reqBody,
+  //   success: () => console.log('success')
+  // });
+
+  // let reqBody = {
+  //   name: file.name,
+  //   size: file.size,
+  //   type: file.type,
+  //   file: csvData
+  // }
+
+  // $scope.grid = {
+  //   data: [
+  //     {
+  //         "firstName": "Cox",
+  //         "lastName": "Carney",
+  //         "company": "Enormo",
+  //         "employed": true
+  //     },
+  //     {
+  //         "firstName": "Lorraine",
+  //         "lastName": "Wise",
+  //         "company": "Comveyer",
+  //         "employed": false
+  //     },
+  //     {
+  //         "firstName": "Nancy",
+  //         "lastName": "Waters",
+  //         "company": "Fuelton",
+  //         "employed": false
+  //     }
+  //   ]
+  // }
