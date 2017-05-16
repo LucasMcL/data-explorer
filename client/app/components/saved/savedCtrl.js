0919ece9abf92638f628f8f3058f517064ad2901
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-16 12:04:56 -0500

    load small grid with no column menus on saved page

--- client/app/components/saved/savedCtrl.js
+++ client/app/components/saved/savedCtrl.js
@@ -1,13 +1,16 @@
-app.controller('SavedCtrl', function($scope, datasets) {
+app.controller('SavedCtrl', function($scope, datasets, uiGridConstants) {
 	console.log('Saved control instantiated')
 
 	$scope.datasets = datasets
 
 	// Trims datasets to 10 rows with first 3 vars
 	datasets.forEach(dataset => {
-		dataset.data = {data: dataset.data.slice(0, 10)}
+		dataset.grid = {data: dataset.data.slice(0, 10)}
+		dataset.grid.enableSorting = false
+		dataset.grid.enableColumnMenus = false
+		dataset.grid.enableVerticalScrollbar = uiGridConstants.scrollbars.NEVER
 		for(let i = 0; i < 10; i++) {
-			let row = dataset.data.data[i]
+			let row = dataset.grid.data[i]
 			let j = 0
 			for(header in row) {
 				if(j <= 2) {j++; continue}
