3e13347d9e0452ee41e8b2c7cccbd0a2b97e74ae
LucasMcL <LMcLaughlin09@gmail.com>
2017-05-17 15:41:05 -0500

    added datasets

--- db/seeds/2.datasets.js
+++ db/seeds/2.datasets.js
@@ -9,7 +9,9 @@ exports.seed = function(knex, Promise) {
         {name: "C02", user_id: 1, data: JSON.stringify(datasets.c02)},
         {name: "Cars", user_id: 1, data: JSON.stringify(datasets.cars)},
         {name: "Motor Trend Cars", user_id: 1, data: JSON.stringify(datasets.mtcars)},
-        {name: "Store Inventory", user_id: 1, data: JSON.stringify(datasets.inventory)}
+        {name: "Store Inventory", user_id: 1, data: JSON.stringify(datasets.inventory)},
+        {name: "Height of Parents and Children", user_id: 1, data: JSON.stringify(datasets.galton)},
+        {name: "Possum Measurements", user_id: 1, data: JSON.stringify(datasets.possum)}
       ]);
     });
 };
